526a82849b6c25c0389d2a3aed39c3dc859cc7ad
marcelogaldino <marcelinhomgo@gmail.com>
2022-02-07 00:30:23 -0300

    feat: SignIn page created

--- App.tsx
+++ App.tsx
@@ -16,6 +16,7 @@ import {
 import theme from './src/global/styles/theme';
 
 import { AppRoutes } from './src/routes/app.routes';
+import { SignIn } from './src/screens/SignIn';
 import { LogBox } from 'react-native';
 import { StatusBar } from 'react-native';
 
@@ -36,7 +37,8 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <NavigationContainer>
         <StatusBar barStyle='light-content' />
-        <AppRoutes />
+        {/* <AppRoutes /> */}
+        <SignIn />
       </NavigationContainer>
     </ThemeProvider>
   )
